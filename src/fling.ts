cbcefc67dead7b42c139f3fee2fe917541605b1d
Ian Johnson <ian@ianjohnson.dev>
2021-12-13 23:02:33 -0500

    No private for now

--- src/fling.ts
+++ src/fling.ts
@@ -1,55 +1,55 @@
 export class FlingManager {
-  readonly #maxInteractionDelay;
-  readonly #maxMeasurements;
-  readonly #minVelocity;
-  #lastTX: [number, number] | null = null;
-  #measurements: number[] = [];
-  #currentFling: Promise<void> | null = null;
+  private readonly _maxInteractionDelay;
+  private readonly _maxMeasurements;
+  private readonly _minVelocity;
+  private _lastTX: [number, number] | null = null;
+  private _measurements: number[] = [];
+  private _currentFling: Promise<void> | null = null;
 
   constructor({
     maxInteractionDelay = 250,
     maxMeasurements = 3,
     minVelocity = 1e-4,
   } = {}) {
-    this.#maxInteractionDelay = maxInteractionDelay;
-    this.#maxMeasurements = maxMeasurements;
-    this.#minVelocity = minVelocity;
+    this._maxInteractionDelay = maxInteractionDelay;
+    this._maxMeasurements = maxMeasurements;
+    this._minVelocity = minVelocity;
   }
 
   cancelFling() {
-    this.#currentFling = null;
+    this._currentFling = null;
   }
 
   fling(delta: (diff: number) => void): Promise<void> {
     const fling = new Promise<void>((resolve) => {
-      if (this.#measurements.length === 0 || this.#lastTX === null) {
+      if (this._measurements.length === 0 || this._lastTX === null) {
         resolve();
         return;
       }
 
       const t = performance.now();
-      if (t - this.#lastTX[0] > this.#maxInteractionDelay) {
-        this.#measurements = [];
-        this.#lastTX = null;
+      if (t - this._lastTX[0] > this._maxInteractionDelay) {
+        this._measurements = [];
+        this._lastTX = null;
         resolve();
         return;
       }
 
       let velocity =
-        this.#measurements.reduce((v1, v2) => v1 + v2) /
-        this.#measurements.length;
-      this.#measurements = [];
-      this.#lastTX = null;
+        this._measurements.reduce((v1, v2) => v1 + v2) /
+        this._measurements.length;
+      this._measurements = [];
+      this._lastTX = null;
 
       let previousTime = t;
       const update = (time: number) => {
-        if (this.#currentFling !== fling) return;
+        if (this._currentFling !== fling) return;
 
         const timeDiff = time - previousTime;
         previousTime = time;
 
         velocity -= velocity / timeDiff;
-        if (Math.abs(velocity) < this.#minVelocity) {
+        if (Math.abs(velocity) < this._minVelocity) {
           velocity = 0;
         }
 
@@ -62,23 +62,23 @@ export class FlingManager {
       };
       requestAnimationFrame(update);
     });
-    this.#currentFling = fling;
+    this._currentFling = fling;
     return fling;
   }
 
   recordPosition(x: number) {
     const t = performance.now();
-    if (this.#lastTX !== null && t > this.#lastTX[0]) {
-      const [lastT, lastX] = this.#lastTX;
-      if (t - lastT <= this.#maxInteractionDelay) {
-        this.#measurements.push((x - lastX) / (t - lastT));
-        if (this.#measurements.length > this.#maxMeasurements) {
-          this.#measurements.splice(0, 1);
+    if (this._lastTX !== null && t > this._lastTX[0]) {
+      const [lastT, lastX] = this._lastTX;
+      if (t - lastT <= this._maxInteractionDelay) {
+        this._measurements.push((x - lastX) / (t - lastT));
+        if (this._measurements.length > this._maxMeasurements) {
+          this._measurements.splice(0, 1);
         }
       } else {
-        this.#measurements = [];
+        this._measurements = [];
       }
     }
-    this.#lastTX = [t, x];
+    this._lastTX = [t, x];
   }
 }
