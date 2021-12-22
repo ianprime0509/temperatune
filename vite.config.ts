6faba03a8897fd3b894275779d83c5f4f0e99b7b
Ian Johnson <ian@ianjohnson.dev>
2021-12-21 19:03:33 -0500

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,45 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      includeAssets: [
+        "favicon.ico",
+        "icon.svg",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Temperatune",
+        short_name: "Temperatune",
+        start_url: "./index.html",
+        display: "standalone",
+        theme_color: "#1e9be9",
+        background_color: "#eeeeee",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "icon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
